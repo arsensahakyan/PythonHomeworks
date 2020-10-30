75e57e1810308a87e1d6f0048a01f6bb4df46faf
arsensahakyan <62113970+arsensahakyan@users.noreply.github.com>
2020-10-31 01:52:38 +0400

    Python homework 6.0 / Ruben&Narek

--- homework 6.0.py
+++ homework 6.0.py
@@ -2,7 +2,7 @@ import string
 
 
 # Narek
-1
+#1
 def colorOfSquare(square):
     if (string.ascii_lowercase.index(square[0].lower()) + 1) % 2 == int(square[1]) % 2:
         return 'black'
@@ -14,6 +14,135 @@ def check(sq1, sq2):
     return colorOfSquare(sq1) == colorOfSquare(sq2)
 
 # 2
+def ek_pos(chess_board):
+    ek_pos = []
+    for row in chess_board:
+        if 'ek' in row:
+            ek_pos.append(chess_board.index(row))
+            ek_pos.append(row.index('q'))
+            break
+    return ek_pos
+
+
+def queen_position(chess_board):
+    queen_pos = []
+    for row in chess_board:
+        if 'q' in row:
+            queen_pos.append(chess_board.index(row))
+            queen_pos.append(row.index('q'))
+            break
+    return queen_pos
+
+
+def horizontal(chessboard, queen_pos):
+    for elem in chessboard[queen_pos[0]]:
+        if str(elem) not in 'qrek':
+            chessboard[queen_pos[0]][chessboard[queen_pos[0]].index(elem)] = 'x'
+    return chessboard
+
+
+def vertical(chessboard, queen_pos):
+    vert = [elem[queen_pos[1]] for elem in chessboard]
+    for elem in vert:
+        if str(elem) not in 'qrek':
+            vert[vert.index(elem)] = 'x'
+    for i in range(8):
+        chessboard[i][queen_pos[1]] = vert[i]
+
+    return chessboard
+
+
+def diagonally1(chessboard, queen_pos):
+    diag = []
+    ind = []
+    if queen_pos[1] - queen_pos[0] < 0:
+        diag = [chessboard[i][i - abs(queen_pos[1] - queen_pos[0])] for i in
+                range(abs(queen_pos[1] - queen_pos[0]), 9 - abs(queen_pos[1] - queen_pos[0]))]
+        ind = [[i, i - abs(queen_pos[1] - queen_pos[0])] for i in range(abs(queen_pos[1] - queen_pos[0]), 8)]
+    elif queen_pos[1] - queen_pos[0] > 0:
+        diag = [chessboard[i][i + abs(queen_pos[1] - queen_pos[0])] for i in
+                range(0, 8 - (queen_pos[1] - queen_pos[0]))]
+        ind = [[i, i + abs(queen_pos[1] - queen_pos[0])] for i in range(0, 8 - (queen_pos[1] - queen_pos[0]))]
+    else:
+        diag = [chessboard[i][i] for i in range(8)]
+        ind = [[i, i] for i in range(8)]
+
+    for elem in diag:
+        if str(elem) not in 'qrek':
+            diag[diag.index(elem)] = 'x'
+
+    for pos in ind:
+        chessboard[pos[0]][pos[1]] = diag[ind.index(pos)]
+
+    return chessboard
+
+
+def diagonally2(chessboard, queen_pos):
+    diag = []
+    ind = []
+    if 0 < queen_pos[0] + queen_pos[1] < 8:
+        diag = [chessboard[i][queen_pos[0] + queen_pos[1] - i] for i in range(queen_pos[0] + queen_pos[1], -1, -1)]
+        ind = [[i, queen_pos[0] + queen_pos[1] - i] for i in range(queen_pos[0] + queen_pos[1], -1, -1)]
+    elif queen_pos[0] + queen_pos[1] > 7:
+        diag = [chessboard[i][queen_pos[0] + queen_pos[1] - i] for i in range(7, queen_pos[0] + queen_pos[1] - 8, -1)]
+        ind = [[i, queen_pos[0] + queen_pos[1] - i] for i in range(7, queen_pos[0] + queen_pos[1] - 8, -1)]
+
+    for elem in diag:
+        if str(elem) not in 'qrek':
+            diag[diag.index(elem)] = 'x'
+    for pos in ind:
+        chessboard[pos[0]][pos[1]] = diag[ind.index(pos)]
+
+    return chessboard
+
+
+def rook(chessboard):
+    rook_pos = []
+    for row in chessboard:
+        for col in row:
+            if col == 'r':
+                rook_pos.append([chessboard.index(row), row.index(col)])
+    for pos in rook_pos:
+        print(f' -- {chessboard[pos[0]]} -- ')
+        for i in chessboard[pos[0]]:
+            if str(i) not in 'qrek':
+                chessboard[pos[0]][chessboard[pos[0]].index(i)] = 'x'
+        for i in chessboard:
+            if str(i[pos[1]]) not in 'qrek':
+                i[pos[1]] = 'x'
+    return chessboard
+
+
+def isMate(chessboard):
+    # rook
+    rook(chessboard)
+
+    # queen position
+    queen_pos = queen_position(chessboard)
+
+    diagonally2(chessboard, queen_pos)
+    horizontal(chessboard, queen_pos)
+    vertical(chessboard, queen_pos)
+    diagonally1(chessboard, queen_pos)
+    print(queen_pos)
+
+    return chessboard
+
+
+chessboard = [
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 'r', 0, 0, 0, 0, 0],
+    [0, 0, 0, 'q', 0, 0, 'ek', 0],
+    ['r', 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0]
+]
+
+cb = isMate(chessboard)
+for elem in cb:
+    print(elem)
 
 
 # Ruben
