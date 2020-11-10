0d205d88bc37c6937ab3b99b8b267665b9c3104f
Arsen Sahakyan <62113970+arsensahakyan@users.noreply.github.com>
2020-11-10 14:04:34 +0400

    Python homework 9.1 / Narek

--- homework 9.2.py
+++ homework 9.2.py
@@ -1,19 +1,17 @@
-# Narek
-
 # 1
-# def fake_coin(list, start, end):
-#     # print(list[start:end+1])
-#     # print(sum(list[start:end+1]))
-#     if sum(list[start:end+1]) == 5 or sum(list[start:end+1]) == 3:
-#         for i in range(start, end+1):
-#             if list[i] == 1:
-#                 return i
-#     else:
-#         start += 3
-#         end1 = end + 3 if(start < 6) else(end + 2)
-#         return fake_coin(list, start, end1)
-#
-# print(fake_coin([2,2,2,2,2,2,2,1], 0, 2))
+def fake_coin(list, start, end):
+    # print(list[start:end+1])
+    # print(sum(list[start:end+1]))
+    if sum(list[start:end+1]) == 5 or sum(list[start:end+1]) == 3:
+        for i in range(start, end+1):
+            if list[i] == 1:
+                return i
+    else:
+        start += 3
+        end1 = end + 3 if(start < 6) else(end + 2)
+        return fake_coin(list, start, end1)
+
+print(fake_coin([2,2,2,2,2,2,2,1], 0, 2))
 
 # 2
 def func(string):
