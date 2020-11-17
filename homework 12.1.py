d929724080059ab90c2db684ccd99fe1992ca82e
Arsen Sahakyan <62113970+arsensahakyan@users.noreply.github.com>
2020-11-17 07:51:46 +0400

    Update homework 12.1.py

--- homework 12.1.py
+++ homework 12.1.py
@@ -11,31 +11,21 @@ def find_length_of_number(num):
             ind[1] += 1
     return ind[1]
 
-print(find_length_of_number(352))
-
-# 2
-# Narek
-# 1
-def find_length_of_number(num):
-    ind = [10, 1]
-
-    while True:
-        if num//ind[0] == 0:
-            break
-        else:
-            ind[0] *= 10
-            ind[1] += 1
-    return ind[1]
 
 print(find_length_of_number(352))
+
 
 # 2
 def func(lst, num):
-    ret_vals = []
+    ret_vals = set()
     for i in range(len(lst)):
-        if num - lst[i] in lst and (lst[i], num-lst[i]) not in ret_vals:
-            ret_vals.append((lst[i], num-lst[i]))
+        test = lst.copy()
+        test.remove(lst[i])
+        if num - lst[i] in test:
+            ret_vals.add((lst[i], num-lst[i]))
+            ret_vals.add((num - lst[i], lst[i]))
+        test = lst.copy()
     return ret_vals
 
 
-print(func([5, 2, 2, 5, 3],8))
+print(*func([5, 2, 2, 5, 3, 4, 4],8))
