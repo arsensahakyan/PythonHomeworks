5aff3c30811d9ba44a784580b699e42e426ea8fc
Arsen Sahakyan <62113970+arsensahakyan@users.noreply.github.com>
2020-12-22 17:54:02 +0400

    Python homework 19.1 / Narek

--- homework 19.1.py
+++ homework 19.1.py
@@ -1,10 +1,13 @@
+import math
+
+
 # Narek
 
 def deg(n):
     degr = n if n < 10 else (n-9)*2+8
     num = 10 ** degr
     for i in range(2, n + 1):
-        degr -= len(str(i))
+        degr -= int(math.log10(i))+1
         num += i*(10**degr)
     print(num)
-deg(13)
+deg(16)
